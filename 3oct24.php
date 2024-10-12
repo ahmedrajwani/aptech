925fc971737ef97aa82966617fc9cca244fc8445
Ahmed Rajwani <hafizahmedrajwani@gmail.com>
2024-10-12 19:45:55 +0500

    Numbers in Php

---
+++ 3oct24.php
@@ -0,0 +1,52 @@
+<?php
+$data = "aptech learning";
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+$data = 2004;
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+$data = 2.4;
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+$data = NULL;
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+$data = [ "Ahmed", "aazib", "Ahsan"];
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+$data = true;
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+// starting function
+$text = 'Aptech Learning'
+// Method 1 
+$length = strlen ($text);
+echo $length , "<br>";
+
+// Method 2 
+echo strlen($text) , "<br>";
+
+echo str_word_count($text) , "<br>";
+
+// Position of 
+
+
+
+
+
+
+
+
+?>
---
+++ Sep232024.php
@@ -0,0 +1,5 @@
+<?php 
+echo "<h1>Php is working</h1>";
+?>
+
+<h1>THIS IS HTML</h1>
